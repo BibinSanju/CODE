a767222e5a9220d45dc0f7f275232b35e3f96841
BibinSanju <bibinsanju18@gmail.com>
2026-07-23 03:05:16 +0530

    i/o style

---
+++ scraper/check.py
@@ -0,0 +1,11 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv('../backend/.env')
+conn = psycopg2.connect(os.getenv('DIRECT_URL') or os.getenv('DATABASE_URL'))
+cur = conn.cursor()
+cur.execute('SELECT "testCases" FROM "Question" WHERE title=\'Merge Intervals\' LIMIT 1')
+print(cur.fetchone()[0])
+cur.close()
+conn.close()
