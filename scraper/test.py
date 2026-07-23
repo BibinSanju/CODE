faa6cda904f159aa463123083d9a11a0f5c5603d
ANIRUDHAN C <cvbanirudhan@gmail.com>
2026-07-23 08:01:30 +0530

    chore: remove unwanted files and temp caches

--- scraper/test.py
+++
@@ -1,7 +0,0 @@
-import psycopg2, os
-from dotenv import load_dotenv
-load_dotenv('../backend/.env')
-conn = psycopg2.connect(os.getenv('DIRECT_URL'))
-cur = conn.cursor()
-cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name='Question'")
-print([row[0] for row in cur.fetchall()])
